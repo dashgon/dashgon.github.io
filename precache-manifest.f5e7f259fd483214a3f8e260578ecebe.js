0b9bf251747a50a062b1c3a60d7513389df21ddc
Soomin Kimu <soominkimu@gmail.com>
2019-06-27 09:01:39 -0700

    NewsAPI

---
+++ precache-manifest.f5e7f259fd483214a3f8e260578ecebe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e95a774398ebe3c80aacd53ceb0728",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b44b357030b927d7e3d",
+    "url": "/static/css/main.da1baf5a.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "7b44b357030b927d7e3d",
+    "url": "/static/js/main.fac7afbb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
